00b64410e22f20b4fa7f56c2a75bb58c8f4f482e
Okan Ozbek <devokan@outlook.com>
2023-09-10 03:34:10 +0200

    Updated camera, added sprinting and refactoring

---
+++ Assets/Scripts/Static/GeneralAnimatorManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Static
+{
+    public class AnimatorSnippets
+    {
+        public static float? Snap(float floatToSnap, IList<Vector2> conditions, IReadOnlyList<float> results, bool includeNegative)
+        {
+            for (int index = 0; index < conditions.Count; index++)
+            {
+                if (floatToSnap > conditions[index].x && floatToSnap < conditions[index].y)
+                {
+                    return results[index];
+                }
+
+                if (!includeNegative) continue;
+                
+                if (floatToSnap < conditions[index].x && floatToSnap > conditions[index].y)
+                {
+                    return results[index];
+                }
+            }
+
+            return null;
+        }
+    }
+}
